cbaa61dee2af823123b4f3a27f189a6601d0fe7e
Yevhenii Shchyhartsov <evgeniy.shigartsov2@gmail.com>
2026-05-18 10:07:51 +0000

    hm-06: fix linter problem, add sample test

--- homeworks/06_tests_code_quality/tests/ballistics_tests.cpp
+++ homeworks/06_tests_code_quality/tests/ballistics_tests.cpp
@@ -1,3 +1,11 @@
 #include "../include/ballistics.hpp"
 
 #include <gtest/gtest.h>
+#include <iostream>
+
+TEST(Ballistics, SampleTest)
+{
+  std::cerr << "-------------H is: " << get_h(1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f) << std::endl;
+
+  EXPECT_NEAR(1, 1, 0.01);
+}
