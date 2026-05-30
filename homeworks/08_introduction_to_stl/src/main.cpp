cf0dae001d9d7e0c1047f948b8be378c05f14a05
Yevhenii Shchyhartsov <evgeniy.shigartsov2@gmail.com>
2026-05-30 12:43:54 +0000

    hm-08: use std::move

--- homeworks/08_introduction_to_stl/src/main.cpp
+++ homeworks/08_introduction_to_stl/src/main.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <map>
 #include "../third_party/json.hpp"
+#include "../include/types.hpp"
 
 #define ENABLE_LOG 1
 #define ENABLE_DEBUG 0
@@ -37,34 +38,6 @@ enum DroneState { STOPPED, ACCELERATING, DECELERATING, TURNING, MOVING };
 enum class SolverType { ANALYTICAL };
 enum class ProviderType { JSON };
 enum class LoaderType { FILE };
-
-struct Coord {
-  float x;
-  float y;
-
-  Coord operator+(const Coord& other) const
-  {
-    Coord result{x + other.x, y + other.y};
-    return result;
-  }
-
-  Coord operator-(const Coord& other) const
-  {
-    Coord result{x - other.x, y - other.y};
-    return result;
-  }
-  Coord operator*(const float scalar) const
-  {
-    Coord result{x * scalar, y * scalar};
-    return result;
-  }
-  Coord operator/(const float divider) const
-  {
-    Coord result{x / divider, y / divider};
-    return result;
-  }
-  bool operator==(const Coord& other) const { return fabsf(x - other.x) < 1e-6f && fabsf(y - other.y) < 1e-6f; }
-};
 
 struct DroneConfig {
   std::string ammoName;
@@ -336,7 +309,7 @@ public:
           });
         }
 
-        targetsInTime.push_back(targetInTime);
+        targetsInTime.push_back(std::move(targetInTime));
       }
       isSuccesFullyLoaded = true;
     }
